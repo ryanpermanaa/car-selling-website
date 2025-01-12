15effda003598fe2ab42e912d2be4d72a464885f
LuxOrionCoding <gungryan1234@gmail.com>
2025-01-12 13:51:24 +0800

    Mini fixes

--- resources/views/components/layouts/footer.blade.php
+++ resources/views/components/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 @props(['footerLinks' => ''])
 
 <footer>
-    <a href="#">Link 1</a>
-    <a href="#">Link 2</a>
+    {{-- <a href="#">Link 1</a>
+    <a href="#">Link 2</a> --}}
     {{ $footerLinks }}
 </footer>
